f42fc568cd93a024871b31a9cc3c1cb8595a6afd
khawla01 <khawla.aafif02@gmail.com>
2024-11-23 18:34:17 +0100

    TP Graphql

---
+++ src/main/java/ma/projet/graph/DTO/TransactionRequest.java
@@ -0,0 +1,17 @@
+package ma.projet.graph.DTO;
+
+import lombok.Data;
+import ma.projet.graph.entities.TypeTransaction;
+
+import java.util.Date;
+
+@Data
+public class TransactionRequest {
+
+    private Long compteId;
+    private double montant;
+    private TypeTransaction type;
+    private Date date;
+
+
+}
---
+++ src/main/java/ma/projet/graph/controllers/CompteControllerGraphQL.java
@@ -0,0 +1,100 @@
+package ma.projet.graph.controllers;
+
+import lombok.AllArgsConstructor;
+import ma.projet.graph.DTO.TransactionRequest;
+import ma.projet.graph.entities.Compte;
+import ma.projet.graph.entities.Transaction;
+import ma.projet.graph.entities.TypeCompte;
+import ma.projet.graph.entities.TypeTransaction;
+import ma.projet.graph.repositories.CompteRepository;
+import ma.projet.graph.repositories.TransactionRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@AllArgsConstructor
+public class CompteControllerGraphQL {
+
+    private CompteRepository compteRepository;
+    private TransactionRepository transactionRepository;
+
+    @QueryMapping
+    public List<Compte> allComptes(){
+        return compteRepository.findAll();
+    }
+
+    @QueryMapping
+    public Compte compteById(@Argument Long id){
+        Compte compte =  compteRepository.findById(id).orElse(null);
+        if(compte == null) throw new RuntimeException(String.format("Compte %s not found", id));
+        else return compte;
+    }
+
+    @QueryMapping
+    public List<Compte> findByType(@Argument TypeCompte type) {
+        List<Compte> comptes = compteRepository.findByType(type);
+
+        if (comptes.isEmpty()) {
+            throw new RuntimeException(String.format("No comptes found for type %s", type));
+        }
+        return comptes;
+    }
+
+    @MutationMapping
+    public Compte saveCompte(@Argument Compte compte){
+        return compteRepository.save(compte);
+    }
+
+    @QueryMapping
+    public Map<String, Object> totalSolde() {
+        long count = compteRepository.count();
+        double sum = compteRepository.sumSoldes();
+        double average = count > 0 ? sum / count : 0;
+
+        return Map.of(
+                "count", count,
+                "sum", sum,
+                "average", average
+        );
+    }
+    @MutationMapping
+    public Transaction addTransaction(@Argument TransactionRequest transactionRequest) {
+        Compte compte = compteRepository.findById(transactionRequest.getCompteId())
+                .orElseThrow(() -> new RuntimeException("Compte not found"));
+
+        Transaction transaction = new Transaction();
+        transaction.setMontant(transactionRequest.getMontant());
+        transaction.setDate(transactionRequest.getDate());
+        transaction.setType(transactionRequest.getType());
+        transaction.setCompte(compte);
+
+        transactionRepository.save(transaction);
+        return transaction;
+    }
+
+
+    @QueryMapping
+    public List<Transaction> compteTransactions(@Argument Long id) {
+        Compte compte = compteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Compte not found"));
+        return transactionRepository.findByCompte(compte);
+    }
+
+    @QueryMapping
+    public Map<String, Object> transactionStats() {
+        long count = transactionRepository.count();
+        Double sumDeposits = transactionRepository.sumByType(TypeTransaction.DEPOT);
+        Double sumWithdrawals = transactionRepository.sumByType(TypeTransaction.RETRAIT);
+
+        return Map.of(
+                "count", count,
+                "sumDeposits", sumDeposits != null ? sumDeposits : 0.0,
+                "sumWithdrawals", sumWithdrawals != null ? sumWithdrawals : 0.0
+        );
+    }
+}
